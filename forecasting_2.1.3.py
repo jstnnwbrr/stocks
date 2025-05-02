1693de58deffe48039d79b02a6b30b56da2a1917
J. Scott Newberry <jstnnwbrr@gmail.com>
2025-05-02 05:52:27 -0600

    Forecast 05.02.25

--- forecasting_2.1.3.py
+++ forecasting_2.1.3.py
@@ -363,14 +363,17 @@ def finalize_forecast_and_metrics(stock_name, rolling_predictions, df, n_periods
     else:
         direction = 'flat'
     
-    max_buy_price = round((predicted_avg_30_days * (1 - (0.75 * predicted_volatility_30_days))), 2)
-    target_sell_price = round((predicted_avg_30_days * (1 + (0.75 * predicted_volatility_30_days))), 2)
+    max_buy_price = round((predicted_avg_30_days * (1 - (0.5 * predicted_volatility_30_days))), 2)
+    target_sell_price = round((predicted_avg_30_days * (1 + (0.5 * predicted_volatility_30_days))), 2)
     
     if direction == 'up':
-        if ((target_sell_price / max_buy_price) - 1) > 0.04 :
-            recommendation = 'buy' 
+        if ((target_sell_price / max_buy_price) - 1) > 0.03:
+            if predicted_volatility_30_days <= 0.10: 
+                recommendation = 'buy'    
+            else:
+                recommendation = 'hold'
         else:
-            recommendation = 'hold'      
+            recommendation = 'avoid/sell'
     else:
         recommendation = 'avoid/sell'
 
@@ -416,75 +419,44 @@ def finalize_forecast_and_metrics(stock_name, rolling_predictions, df, n_periods
 # RUN APP
 ########################
 # Specify stock names
-stock_list = ['ECL',
-            'ED',
+stock_list = ['AGNC',
+              'BTG',
+              'CCL',
+              'CZR',
+              'DD',
+              'DXCM',
+              'HSY',
+              'EXC',
+              'CCK',
+              'HSAI',
+            'ECL',
             'EMN',
             'ES',
-            'ETN',
-            'EXC',
-            'FAST',
-            'FDX',
             'FSLR',
-            'GEV',
-            'GOOG',
             'GPN',
-            'GS',
-            'HCA',
-            'HSY',
             'HUBB',
-            'IBM',
             'IFF',
             'IQV',
             'JBHT',
             'KKR',
-            'LH',
-            'LLY',
             'LUV',
-            'MA',
-            'MAA',
-            'MAS',
-            'META',
-            'MKC',
             'MKTX',
-            'MMC',
-            'MMM',
-            'MOH',
             'MRNA',
-            'MTCH',
-            'MTD',
-            'NEM',
-            'NFLX',
-            'NTAP',
             'NVDA',
             'NXPI',
-            'O',
             'ON',
-            'PEG',
-            'PH',
             'PM',
-            'PNC',
-            'PODD',
             'POOL',
             'PWR',
-            'ROK',
-            'SJM',
             'SMCI',
             'SOLV',
             'SPGI',
-            'STLD',
-            'SW',
-            'TDG',
             'TER',
-            'TFC',
             'TMUS',
             'TPL',
-            'TRMB',
-            'TSCO',
             'TT',
             'UNH',
             'UNP',
-            'VZ',
-            'WEC',
             'YUM']
 
 
